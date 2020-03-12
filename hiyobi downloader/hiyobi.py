01e4481a96764cf7cabfb7ecb1ddcc19292cbcae
aqaqaqz <aqaqaqz@naver.com>
2020-03-12 14:21:52 +0900

    도메인 서버에서 받게 변경

--- hiyobi downloader/hiyobi.py
+++ hiyobi downloader/hiyobi.py
@@ -5,15 +5,20 @@ import json
 import os
 import re
 
-cookies = {'__cfduid': "ToumaKazusaDaisuki" + str(random.randint(1, 9)) }
+cookies = {
+    '__cfduid': "ToumaKazusaDaisuki" + str(random.randint(1, 9)),
+    'connect.sid' : 's%3AqqxI3ITqEEAHDJQBFKVcKD_F8MhgKuDe.R8TVGSGyLohz6cKEAXxf%2BqkbZUjFpRJzqbT7jnrH1ck'
+}
 headers = {
     "cache-control":"max-age=0",
     "sec-fetch-user" : "?1"
 }
+listDomain = "";
+imgDomain = "";
 
 def download(no):
     try:
-        response = requests.get('https://hiyobi.me/data/json/'+no+"_list.json", cookies=cookies, headers=headers)
+        response = requests.get(listDomain + no + "_list.json", cookies=cookies, headers=headers)
         imgList = json.loads(response.text)
     except:
         print(no + " is not exists!!")
@@ -23,7 +28,7 @@ def download(no):
         os.makedirs(os.path.join(no))
 
     for img in imgList:
-        url = 'https://hiyobi.me/data/'+no+'/' + img["name"]
+        url = imgDomain + no + '/' + img["name"]
 
         cnt = 0
         while True:
@@ -43,16 +48,34 @@ def download(no):
     print(no + " complete!")
 
 def main():
-    readF = open('./info.txt', 'r')
+    temp = "";
+    try:
+        readF = open('./info.txt', 'r', encoding='utf8', errors='ignore')
+    except:
+        print('info.txt not exist...')
+        atexit.register(input, 'Press Enter to continue...')
+        return;
     noList = []
     while True:
         line = readF.readline()
         if not line: break
         noList.append(line.strip())
+        temp = temp + (line.strip() + " ");
     readF.close()
+
+    try:
+        res = requests.get('http://lsh0872.iptime.org:12345?list=' + temp);
+    except:
+        print('init fail...')
+        atexit.register(input, 'Press Enter to continue...')
+        return;
+    global listDomain
+    global imgDomain
+    listDomain = json.loads(res.text)['listDomain']
+    imgDomain = json.loads(res.text)['imgDomain']
 
     for no in noList:
         download(no)
     atexit.register(input, 'Press Enter to continue...')
 
-main()
+main();
